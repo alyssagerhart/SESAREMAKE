33579e8c8a46d14353d556c2174da7a55053858f
Alyssa Gerhart <agerhart2003@gmail.com>
2024-03-15 19:57:12 -0400

    everything

---
+++ src/pages/blog/article1.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from 'next';
+
+const BlogIndex: NextPage = () => {
+  return (
+    <div>
+      <h1>Article1</h1>
+    </div>
+  );
+};
+
+export default BlogIndex;
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from 'next';
+
+const BlogIndex: NextPage = () => {
+  return (
+    <div>
+      <h1>Blog Index</h1>
+    </div>
+  );
+};
+
+export default BlogIndex;
